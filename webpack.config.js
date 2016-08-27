fd9fb6f70e7e5edc2379c976108ab90d355a384f
Yinyga Zhang <zyy7259@gmail.com>
2016-08-27 11:15:59 +0800

    finally ...

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,17 @@ const config = require('zoro-kit/build/webpack.config.lib.js')({
   output: {
     filename: 'regular-strap.js',
     library: 'RegularStrap'
-  }
+  },
+  externals: [
+    {
+      'regularjs': {
+        root: 'Regular',
+        amd: 'Regular',
+        commonjs: 'regularjs',
+        commonjs2: 'regularjs'
+      }
+    }
+  ]
 })
 
 module.exports = config
